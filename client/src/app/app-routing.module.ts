863ad44927158c900c8f426cba31ad9fde48d550
Kevin Tou <toukevin@gmail.com>
2020-06-02 19:50:14 -0700

    Added routes to recipe and shopping list

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { RecipesComponent } from "./components/recipes/recipes.component";
+import { ShoppingListComponent } from "./components/shopping-list/shopping-list.component";
+import { ShoppingEditComponent } from "./components/shopping-list/shopping-edit/shopping-edit.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "/recipes" },
+  { path: "recipes", component: RecipesComponent },
+  {
+    path: "shopping-list",
+    component: ShoppingListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
